a9808e5d01218f5245f4e4e53ced783e2c3204ff
unknown <pvillela-2021656@kinal.edu.gt>
2025-03-04 22:51:13 -0600

    Se crea lista de funcionalidades hechas y no hechas(Uso personal)

---
+++ FuncionalidadesLista.js
@@ -0,0 +1,28 @@
+/*FUNCIONALIDADES ADMINISTRADOR:
+*-Product:
+*   -Crear Producto
+*   -Listar Productos
+*   -Delete Productos
+*   -Update Productos
+*   -Listar Productos sin stock
+*   -Listar Productos más vendidos
+*   -Listar Producto en especifico
+*PENDIENTES:
+*   -Restar stock al hacer compra
+*-Category:
+*   -Crear Categoria
+*   -Listar Categorias
+*   -Delete Categorias
+*   -Update Categorias
+*PENDIENTES:
+*   -Cambiar de categoria a producto al ser eliminada la categoria
+*/
+
+/*FUNCIONALIDADES CLIENTE:
+*-User:
+*   -Crear Usuario
+*   -Login de Cliente
+*   -Login de admin
+*/
+
+//EXTERIOR: CREAR SWAGGER Y DOCUMENTACION, ENDPOINTS DE POSTMAN Y INFO DE MONGODB
